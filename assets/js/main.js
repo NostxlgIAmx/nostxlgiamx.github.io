235ee3044d05a53bcb8133c7395ab21ddd922c5a
NostxlgIAmx <NostxlgIA@proton.me>
2026-09-09 20:24:34 -0600

    Actualiza versión canónica de assets tras corrección móvil

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 
   const currentScript = document.currentScript;
   const assetBase = currentScript ? new URL('.', currentScript.src) : new URL('./assets/js/', location.href);
-  const VERSION = '20260910-refactor5';
+  const VERSION = '20260910-refactor6';
 
   const ensureScript = (src, marker) => {
     if (document.querySelector(`script[data-${marker}]`)) return;
